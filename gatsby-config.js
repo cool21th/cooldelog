4c3ce1df808433df7134120c8327e9738ad08372
cool21th <cool21th@naver.com>
2020-03-19 23:16:16 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
   /* Your site config here */
   siteMetadata: {
     title: `Coolog`,
-    description: 'A starter blog demonstrating what Gatsby can do.`,
+    description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://coolog.netlify.com/`,
     home: {
       title: `HI! I'm Coo`,
